cee5431a9a62ae6304898a0b3528a106ef80d951
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-12-09 23:21:02 +0500

    Task1

--- Tyuiu.MitrofanovaPV.Sprint6.Task1.V25.Lib/Class1.cs
+++ Tyuiu.MitrofanovaPV.Sprint6.Task1.V25.Lib/Class1.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.MitrofanovaPV.Sprint6.Task1.V25.Lib
             int count = 0;
             for (int x = startValue; x <= stopValue; x++)
             {
-                y = ((5*x + 2.5)/(Math.Sin(x) -2)) + 2;
+                y = Math.Round(((5*x + 2.5)/(Math.Sin(x) -2)) + 2,2);
                 valarr[count] = y;
 
                 count++;
