9cdd1f24251df27d9bb16bb1070d8c7c2cd6acb9
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-12-09 21:04:39 +0500

    Add project files.

---
+++ Tyuiu.MitrofanovaPV.Sprint6.Task0.V25.Test/Test1.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint6.Task0.V25.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint6.Task0.V25.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds =new DataService();
+            double res = ds.Calculate(1);
+            double wait = 1;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.MitrofanovaPV.Sprint6.Task0.V25/Form1.cs
@@ -0,0 +1,12 @@
+using Tyuiu.MitrofanovaPV.Sprint6.Task0.V25.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint6.Task0.V25
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
